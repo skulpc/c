85ffbbc615860522e05d7981d6fb4d9159ccb477
phuid <75166353+phuid@users.noreply.github.com>
2024-12-17 14:31:54 +0100

    stack_functions

--- zasobnik/main.c
+++ zasobnik/main.c
@@ -45,6 +45,68 @@ int pop(Stack *stack)
   return val;
 }
 
+Node *get_node(Stack *stack, int index)
+{
+  Node *tmp = stack->top;
+  for (int i = 0; i < index; i++)
+  {
+    if (tmp == NULL)
+    {
+      return -1;
+    }
+    tmp = tmp->next;
+  }
+  return tmp;
+}
+
+int get_val(Stack *stack, int index)
+{
+  Node *tmp = get_node(stack, index);
+  if (tmp == -1)
+  {
+    return -1;
+  }
+  return tmp->val;
+}
+
+void set_val(Stack *stack, int index, int val)
+{
+  Node *tmp = get_node(stack, index);
+  if (tmp == -1)
+  {
+    return;
+  }
+  tmp->val = val;
+}
+
+void insert(Stack *stack, int index, int val)
+{
+  Node *new_node = (Node *)malloc(sizeof(Node));
+  if (new_node == NULL)
+  {
+    printf("Nedostatek pameti\n");
+    return;
+  }
+
+  new_node->val = val;
+
+  if (index == 0)
+  {
+    new_node->next = stack->top;
+    stack->top = new_node;
+    return;
+  }
+
+  Node *prev = get_node(stack, index - 1);
+  if (prev == -1)
+  {
+    return;
+  }
+
+  new_node->next = prev->next;
+  prev->next = new_node;
+}
+
 void free_stack(Stack *stack)
 {
   while (stack->top != NULL)

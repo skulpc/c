819ee7262cf63b713caf5336aa14c3e7adcd2828
phuid <75166353+phuid@users.noreply.github.com>
2025-01-28 14:31:28 +0100

    bubble n shaker sort

--- sorting/main.c
+++ sorting/main.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 
 void print_arr(int arr[], int size);
 void print_str_arr(char *str, int arr[], int size);
 void print_arr_with_wall(int wall, int arr[], int size);
+void print_arr_with_char(int wall, char c, int arr[], int size);
+void shuffle(int arr[], int size);
+
 void selection_sort(int arr[], int size);
 void insert_sort(int arr[], int size);
+void bubble_sort(int arr[], int size);
+void shaker_sort(int arr[], int size);
+
 int move_one_element(int from, int to, int arr[], int size);
-void shuffle(int arr[], int size);
 
 int main()
 {
@@ -33,14 +39,28 @@ int main()
   printf("insert sort:\n");
   insert_sort(arr, SIZE(arr));
   print_arr(arr, SIZE(arr));
+
+  shuffle(arr, SIZE(arr));
+
+  printf("bubble sort:\n");
+  bubble_sort(arr, SIZE(arr));
+  print_arr(arr, SIZE(arr));
+
+  shuffle(arr, SIZE(arr));
+
+  printf("shaker sort:\n");
+  shaker_sort(arr, SIZE(arr));
+  print_arr(arr, SIZE(arr));
 }
 
 void shuffle(int arr[], int size)
 {
+  srand(time(NULL));
   for (size_t i = 0; i < size; i++)
   {
     int r = rand() % size;
-    move_one_element(i, r, arr, size);
+    // move_one_element(i, r, arr, size);
+    arr[i] = r + 1;
   }
 }
 
@@ -78,6 +98,91 @@ void insert_sort(int arr[], int size)
         move_one_element(i, u, arr, size);
         break;
       }
+    }
+  }
+}
+
+void bubble_sort(int arr[], int size)
+{
+  for (size_t i = 0; i < size - 1; i++)
+  {
+    print_arr_with_wall(size - i, arr, size);
+
+    int switched = -1;
+    for (size_t u = 1; u < size - i; u++)
+    {
+      if (arr[u - 1] > arr[u])
+      {
+        int tmp = arr[u - 1];
+        arr[u - 1] = arr[u];
+        arr[u] = tmp;
+
+        switched = u;
+      }
+    }
+    if (switched == -1)
+    {
+      break;
+    }
+    else
+    {
+      i = size - (switched + 1);
+    }
+  }
+}
+
+// (double sided bubble sort)
+void shaker_sort(int arr[], int size)
+{
+  int high_zone = size;
+  int low_zone = -1;
+
+  while (1)
+  {
+    print_arr_with_char(high_zone, '(', arr, size);
+
+    int switched = low_zone;
+    for (size_t u = low_zone + 2; u < high_zone; u++)
+    {
+      if (arr[u - 1] > arr[u])
+      {
+        int tmp = arr[u - 1];
+        arr[u - 1] = arr[u];
+        arr[u] = tmp;
+
+        switched = u;
+      }
+    }
+    if (switched == low_zone)
+    {
+      break;
+    }
+    else
+    {
+      high_zone = switched;
+    }
+
+    print_arr_with_char(low_zone, ')', arr, size);
+
+    switched = high_zone;
+    for (size_t u = high_zone - 1; u > low_zone + 1; u--)
+    {
+      if (arr[u - 1] > arr[u])
+      {
+        int tmp = arr[u - 1];
+        arr[u - 1] = arr[u];
+        arr[u] = tmp;
+
+        switched = u;
+      }
+    }
+    if (switched == high_zone)
+    {
+      break;
+    }
+    else
+    {
+      low_zone = switched - 1;
     }
   }
 }
@@ -148,6 +253,23 @@ void print_arr_with_wall(int wall, int arr[], int size)
     if (i + 1 == wall)
     {
       printf("|");
+    }
+    else
+    {
+      printf(" ");
+    }
+  }
+  printf("\n");
+}
+
+void print_arr_with_char(int wall, char c, int arr[], int size)
+{
+  for (size_t i = 0; i < size; i++)
+  {
+    printf("%d", arr[i]);
+    if (i + 1 == wall)
+    {
+      printf("%c", c);
     }
     else
     {
